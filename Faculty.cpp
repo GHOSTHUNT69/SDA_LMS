32eb40db3baba073f3c11a1d91f3e921b0bebc0e
usama aslam <Usama.Aslam.pc@outlook.com>
2020-12-20 22:15:01 +0500

    faculty

--- Faculty.cpp
+++ Faculty.cpp
@@ -8,3 +8,12 @@ Faculty::Faculty(string name_, string dob_, string email_)
 	:Person(name_,dob_,email_)
 {
 }
+
+Faculty::Faculty(string name_, string dob_, string email_, vector<Section*> fsections_)
+        :Person(name_, dob_, email_), fsections(fsections_)
+{
+}
+
+Faculty::~Faculty()
+{
+}
