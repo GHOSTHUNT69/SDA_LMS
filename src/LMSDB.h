7480f0e123464f60f7fa90282671cd537f80124e
ghosthunt <50262751+GHOSTHUNT69@users.noreply.github.com>
2020-12-24 21:13:51 +0500

    sqlite3 added

--- src/LMSDB.h
+++ src/LMSDB.h
@@ -1,8 +1,21 @@
+//https://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
 #pragma once
+#include"sqlite3.h"
+
+/*
+* functions to
+* initialize tables
+* add data
+* remove data
+* update data
+* retrive data
+* data is -Students, Teachers, Acdemic Officers, Registrations, Courses, Attendance etc
+*/
 
 namespace LMS {
 	class LMSDB
 	{
+		
 
 	};
 }
