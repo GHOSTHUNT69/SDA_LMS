4018088eae2ad2f6a1dc7714bddd92a14a7d9144
ghosthunt <50262751+GHOSTHUNT69@users.noreply.github.com>
2020-12-26 04:10:18 +0500

    LMSDB errors

--- src/LMSDB.h
+++ src/LMSDB.h
@@ -1,43 +1,43 @@
-// For help:https://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
-// For viewing the sql data and table(you can also write queries just like Microsoft SQL)
-// https://sqlitebrowser.org/dl/
-#pragma once
-#include"Student_fwd.h"
-#include"sqlite3.h"
-#include<string>
-#include<vector>
-using std::vector;
-using std::string;
-
-/*
-* functions to
-* initialize tables
-* add data
-* remove data
-* update data
-* retrive data
-* data is -Students, Teachers, Acdemic Officers, Registrations, Courses, Attendance etc
-*/
-
-namespace LMS {
-	class LMSDB
-	{
-		sqlite3* db;
-	public:
-		LMSDB();
-		~LMSDB();
-		void initializeTables();
-		void storeStudents();
-		void storeFaculty();
-		void storeCourses();
-		//store faculty
-		//store courses etc
-
-		vector<Student*> retriveStudents();
-		//etc
-
-		static int callback(void* NotUsed, int argc, char** argv, char** azColName);
-
-	};
-}
-
+//// For help:https://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
+//// For viewing the sql data and table(you can also write queries just like Microsoft SQL)
+//// https://sqlitebrowser.org/dl/
+//#pragma once
+//#include"Student_fwd.h"
+//#include"sqlite3.h"
+//#include<string>
+//#include<vector>
+//using std::vector;
+//using std::string;
+//
+///*
+//* functions to
+//* initialize tables
+//* add data
+//* remove data
+//* update data
+//* retrive data
+//* data is -Students, Teachers, Acdemic Officers, Registrations, Courses, Attendance etc
+//*/
+//
+//namespace LMS {
+//	class LMSDB
+//	{
+//		sqlite3* db;
+//	public:
+//		LMSDB();
+//		~LMSDB();
+//		void initializeTables();
+//		void storeStudents();
+//		void storeFaculty();
+//		void storeCourses();
+//		//store faculty
+//		//store courses etc
+//
+//		vector<Student*> retriveStudents();
+//		//etc
+//
+//		static int callback(void* NotUsed, int argc, char** argv, char** azColName);
+//
+//	};
+//}
+//
